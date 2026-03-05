c025a97bd2e4fc9b64af264721ac4ca6db8ff212
Camilly Ferreira <camilly.ferreira@aluno.ifsp.edu.br>
2026-03-05 17:53:36 -0300

    Navbar creation with tailwind

---
+++ guinea-store/src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { DM_Sans, Golos_Text } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+import NavBar from "@/components/NavBar/NavBar";
+import MiddleNav from "@/components/NavBar/NavBar";
+
+const dmSans = DM_Sans({
+  weight: "400",
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+const golosText = Golos_Text({
+  weight: "400",
+  variable: "--font-golos-text",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Guinea Store",
+  description: "e-commerce moderno e profissional para produtos de porquinhos-da-índia.",
+};
+
+const waffleMango = localFont({
+  src: [
+    {
+      path: "../../public/waffle-mango/Waffle-Mango.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    
+  ],
+  variable: "--font-waffle-mango",
+})
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${dmSans.variable} ${golosText.variable} ${waffleMango.variable}`}
+      >
+        <NavBar />
+
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ guinea-store/src/components/NavBar/MiddleNav.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function MiddleNav() {
+
+    return (
+
+         <div className="w-full bg-[var(--primary)] border-b border-gray-300 relative">
+            <div className="flex items-center justify-between py-3 px-[8%] lg:px-[16%]">
+
+                {/* Logo */}
+                <Link href='/' className="text-4xl lg:text-5xl font-bold font-sans text-white">
+                     Guinea<span className="text-[var(--secondary)]"> Store</span>
+                </Link>
+
+            </div>
+         </div>
+
+    )
+}
+
